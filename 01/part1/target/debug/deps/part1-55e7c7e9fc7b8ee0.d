112398fe152b8aa2deaf63133d8409d6f3f22482
Michael Churilov <michael.churilov@gmail.com>
2023-12-01 20:50:11 -0500

    Adding gitignore

--- 01/part1/target/debug/deps/part1-55e7c7e9fc7b8ee0.d
+++
@@ -1,6 +0,0 @@
-/Users/mike/code/advent-of-code-2023/01/part1/target/debug/deps/part1-55e7c7e9fc7b8ee0: src/main.rs src/input.txt
-
-/Users/mike/code/advent-of-code-2023/01/part1/target/debug/deps/part1-55e7c7e9fc7b8ee0.d: src/main.rs src/input.txt
-
-src/main.rs:
-src/input.txt:
--- 01/part1/target/debug/deps/part1-b61e9e5d9430f688.d
+++
@@ -1,5 +0,0 @@
-/Users/mike/code/advent-of-code-2023/01/part1/target/debug/deps/libpart1-b61e9e5d9430f688.rmeta: src/main.rs
-
-/Users/mike/code/advent-of-code-2023/01/part1/target/debug/deps/part1-b61e9e5d9430f688.d: src/main.rs
-
-src/main.rs:
