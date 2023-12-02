e40ad2a0bc6d1c117440684346a433b6d2da4621
Michael Churilov <michael.churilov@gmail.com>
2023-12-01 20:53:13 -0500

    Delete build artificats

--- 01/part2/target/debug/deps/part2-82f680c9bdf13b7a.d
+++
@@ -1,6 +0,0 @@
-/Users/mike/code/advent-of-code-2023/01/part2/target/debug/deps/part2-82f680c9bdf13b7a: src/main.rs src/../../input.txt
-
-/Users/mike/code/advent-of-code-2023/01/part2/target/debug/deps/part2-82f680c9bdf13b7a.d: src/main.rs src/../../input.txt
-
-src/main.rs:
-src/../../input.txt:
--- 01/part2/target/debug/part2.d
+++
@@ -1,1 +0,0 @@
-/Users/mike/code/advent-of-code-2023/01/part2/target/debug/part2: /Users/mike/code/advent-of-code-2023/01/part2/src/../../input.txt /Users/mike/code/advent-of-code-2023/01/part2/src/main.rs
