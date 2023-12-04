ec2eb06eaadb200b1ba1b7b065b68437e96e98bc
Michael Churilov <michael.churilov@gmail.com>
2023-12-04 18:36:34 -0500

    slightly optimized

--- day04/part2/src/main.rs
+++ day04/part2/src/main.rs
@@ -1,13 +1,5 @@
 use regex::Regex;
 
-#[derive(Debug)]
-struct Card {
-    id: String,
-    wins: u32,
-    player_numbers: Vec<u32>,
-    winning_numbers: Vec<u32>,
-}
-
 fn main() {
     let input = include_str!("../../input.txt");
     //     let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -17,16 +9,14 @@ fn main() {
     // Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
     // Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
-    let mut cards: Vec<Card> = Vec::new();
-    let mut card_amounts: Vec<u32> = Vec::new();
+    let mut wins: Vec<u32> = Vec::new();
+    let mut amounts: Vec<u32> = Vec::new();
+
+    let regex = Regex::new(r"(?m)\d+").unwrap();
 
     for line in input.lines() {
         let parts = line.split(':').collect::<Vec<&str>>();
-        let id = parts[0].trim();
-
         let numbers = parts[1].split('|').collect::<Vec<&str>>();
-
-        let regex = Regex::new(r"(?m)\d+").unwrap();
         let player_numbers = regex
             .captures_iter(numbers[0])
             .map(|c| c[0].parse::<u32>().unwrap())
@@ -36,30 +26,21 @@ fn main() {
             .map(|c| c[0].parse::<u32>().unwrap())
             .collect::<Vec<u32>>();
 
-        let mut wins = 0;
+        let mut card_wins = 0;
         for pn in player_numbers.iter() {
             if winning_numbers.contains(pn) {
-                wins += 1
+                card_wins += 1
             }
         }
 
-        let card = Card {
-            id: id.to_string(),
-            wins,
-            player_numbers,
-            winning_numbers,
-        };
-
-        cards.push(card);
-        card_amounts.push(1);
+        wins.push(card_wins);
+        amounts.push(1);
     }
 
-    let length = cards.len();
+    let length = wins.len();
     for id in 0..length {
-        let card = &cards[id];
-
-        let until = card.wins;
-        let amount = card_amounts[id];
+        let until = wins[id];
+        let amount = amounts[id];
 
         for _ in 0..amount {
             for i in id..(id + (until as usize)) {
@@ -68,10 +49,10 @@ fn main() {
                     break;
                 }
 
-                card_amounts[index] += 1;
+                amounts[index] += 1;
             }
         }
     }
 
-    println!("Total sum: {}", card_amounts.iter().sum::<u32>());
+    println!("Total sum: {}", amounts.iter().sum::<u32>());
 }
