5f93b5f72be13d61cf41c76f23bdfdb3eb5e8f88
Michael Churilov <michael.churilov@gmail.com>
2023-12-05 21:29:35 -0500

    Got part 1...somehow

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,6 +1,27 @@
-use std::collections::HashMap;
-
 use regex::Regex;
+
+#[derive(Debug)]
+struct Range {
+    start: u64,
+    end: u64,
+    to: u64,
+}
+
+#[derive(Debug)]
+struct Map {
+    name: String,
+    ranges: Vec<Range>,
+}
+
+impl Range {
+    fn get_to(&self, from: u64) -> u64 {
+        if from >= self.start && from <= self.end {
+            let diff = from - self.start;
+            return self.to + diff;
+        }
+        from
+    }
+}
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -42,16 +63,38 @@ fn main() {
     let number_regex = Regex::new(r"\d+").unwrap();
     let seeds = number_regex
         .captures_iter(lines.next().unwrap_or(""))
-        .map(|c| c[0].parse::<u32>().unwrap_or(0))
-        .collect::<Vec<u32>>();
+        .map(|c| c[0].parse::<u64>().unwrap_or(0))
+        .collect::<Vec<u64>>();
 
-    let mut seed_to_soil_map: HashMap<u32, u32> = HashMap::new();
-    let mut soil_to_fertilizer_map: HashMap<u32, u32> = HashMap::new();
-    let mut fertilizer_to_water_map: HashMap<u32, u32> = HashMap::new();
-    let mut water_to_light_map: HashMap<u32, u32> = HashMap::new();
-    let mut light_to_temperature_map: HashMap<u32, u32> = HashMap::new();
-    let mut temperature_to_humidity_map: HashMap<u32, u32> = HashMap::new();
-    let mut humidity_to_location_map: HashMap<u32, u32> = HashMap::new();
+    let mut seed_to_soil_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut soil_to_fertilizer_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut fertilizer_to_water_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+
+    let mut water_to_light_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut light_to_temperature_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut temperature_to_humidity_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut humidity_to_location_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
 
     let mut all_maps = vec![
         &mut seed_to_soil_map,
@@ -67,29 +110,32 @@ fn main() {
 
     let mut current_map_index: usize = 0;
     for line in lines {
+        let current_map = &mut all_maps[current_map_index];
         if line.is_empty() {
             current_map_index += 1;
             continue;
         }
 
         if line.contains("map") {
+            current_map.name = line.to_string();
             continue;
         }
 
         let numbers = number_regex
             .captures_iter(line)
-            .map(|c| c[0].parse::<u32>().unwrap_or(0))
-            .collect::<Vec<u32>>();
-
-        let current_map = &mut all_maps[current_map_index];
+            .map(|c| c[0].parse::<u64>().unwrap_or(0))
+            .collect::<Vec<u64>>();
 
         let target_num = numbers[0];
         let source_num = numbers[1];
-        let range = numbers[2];
+        let til = numbers[2];
 
-        for i in 0..range {
-            current_map.insert(source_num + i, target_num + i);
-        }
+        let range = Range {
+            start: source_num,
+            end: source_num + til,
+            to: target_num,
+        };
+        current_map.ranges.push(range);
     }
 
     // println!("Seeds");
@@ -97,18 +143,36 @@ fn main() {
 
     // println!("Maps");
     // for map in all_maps {
-    // println!("{:?}", map);
+    //     println!("{:?}", map);
     // }
 
-    // println!("{}", lines.next().unwrap_or("Nothing!"));
-
+    let mut lowest_destination: Option<u64> = None;
     for seed in seeds {
-        let mut location: &u32 = &seed;
+        let mut location = seed;
         println!("Starting at {}", location);
         for map in &mut all_maps {
-            location = map.get(location).unwrap_or(location);
+            // println!("Map: {:?}", map);
+            'range: for range in &mut map.ranges {
+                let current = range.get_to(location);
+
+                if current != location {
+                    location = current;
+                    break 'range;
+                }
+            }
         }
 
         println!("Ended in: {}", location);
+
+        if lowest_destination.is_none() {
+            lowest_destination = Some(location);
+            continue;
+        }
+
+        if location < lowest_destination.unwrap() {
+            lowest_destination = Some(location);
+        }
     }
+
+    println!("Lowest destination: {}", lowest_destination.unwrap());
 }
