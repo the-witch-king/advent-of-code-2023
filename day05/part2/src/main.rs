6f95945bc27657f68d9489f073e6d0af421abaeb
Michael Churilov <michael.churilov@gmail.com>
2023-12-07 18:46:39 -0500

    Sorta working day5p2

--- day05/part2/src/main.rs
+++ day05/part2/src/main.rs
@@ -1,29 +1,30 @@
 use regex::Regex;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Range {
     start: u64,
     end: u64,
-    shift_by: i64,
+    to: u64,
 }
 
-// #[derive(Debug)]
-// struct Map {
-//     ranges: Vec<Range>,
-// }
+#[derive(Debug)]
+struct Map {
+    name: String,
+    ranges: Vec<Range>,
+}
 
 impl Range {
     fn has(&self, num: u64) -> bool {
         num >= self.start && num <= self.end
     }
 
-    // fn get_to(&self, from: u64) -> u64 {
-    //     if from >= self.start && from <= self.end {
-    //         let diff = from - self.start;
-    //         return self.shift_by + diff;
-    //     }
-    //     from
-    // }
+    fn get_to(&self, from: u64) -> u64 {
+        if from >= self.start && from <= self.end {
+            let diff = from - self.start;
+            return self.to + diff;
+        }
+        from
+    }
 }
 
 fn main() {
@@ -78,61 +79,59 @@ fn main() {
     println!("{}", seeds.len());
     // println!("{:?}", seeds);
 
-    // let mut seed_to_soil_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    // let mut soil_to_fertilizer_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    // let mut fertilizer_to_water_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    //
-    // let mut water_to_light_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    // let mut light_to_temperature_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    // let mut temperature_to_humidity_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    // let mut humidity_to_location_map: Map = Map {
-    //     name: "".to_string(),
-    //     ranges: Vec::new(),
-    // };
-    //
-    // let mut all_maps = vec![
-    //     &mut seed_to_soil_map,
-    //     &mut soil_to_fertilizer_map,
-    //     &mut fertilizer_to_water_map,
-    //     &mut water_to_light_map,
-    //     &mut light_to_temperature_map,
-    //     &mut temperature_to_humidity_map,
-    //     &mut humidity_to_location_map,
-    // ];
-    //
-    let mut all_maps: Vec<Vec<Range>> = vec![Vec::new(); 7];
+    let mut seed_to_soil_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut soil_to_fertilizer_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut fertilizer_to_water_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+
+    let mut water_to_light_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut light_to_temperature_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut temperature_to_humidity_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+    let mut humidity_to_location_map: Map = Map {
+        name: "".to_string(),
+        ranges: Vec::new(),
+    };
+
+    let mut all_maps = vec![
+        &mut seed_to_soil_map,
+        &mut soil_to_fertilizer_map,
+        &mut fertilizer_to_water_map,
+        &mut water_to_light_map,
+        &mut light_to_temperature_map,
+        &mut temperature_to_humidity_map,
+        &mut humidity_to_location_map,
+    ];
 
     let number_regex = Regex::new(r"\d+").unwrap();
     lines.next(); // We know there's an empty line after the seeds
 
-    let mut current_map: Vec<Range> = Vec::new();
+    let mut current_map_index: usize = 0;
     for line in lines {
+        let current_map = &mut all_maps[current_map_index];
         if line.is_empty() {
-            all_maps.push(current_map);
-            current_map = Vec::new();
+            current_map_index += 1;
             continue;
         }
 
         if line.contains("map") {
-            // current_map.name = line.to_string();
+            current_map.name = line.to_string();
             continue;
         }
 
@@ -148,9 +147,9 @@ fn main() {
         let range = Range {
             start: source_num,
             end: source_num + til,
-            shift_by: (source_num as i64) - (target_num as i64),
+            to: target_num,
         };
-        current_map.push(range);
+        current_map.ranges.push(range);
     }
 
     // println!("Seeds");
@@ -161,28 +160,26 @@ fn main() {
     //     println!("{:?}", map);
     // }
 
-    let mut lowest_destination: Option<u64> = None;
+    let mut end_locations: Vec<u64> = Vec::new();
     for seed in seeds {
         let mut location = seed;
         // println!("Starting at {}", location);
         for map in &mut all_maps {
             // println!("Map: {:?}", map);
-            'range: for range in map {
+            'range: for range in &mut map.ranges {
                 if range.has(location) {
-                    // location = range.get_to(location);
-                    location = ((location as i64) - range.shift_by) as u64;
-
+                    location = range.get_to(location);
                     break 'range;
                 }
             }
         }
 
-        if lowest_destination.is_none() || location < lowest_destination.unwrap() {
-            lowest_destination = Some(location);
-        }
-
         // println!("Ended in: {}", location);
+        end_locations.push(location);
     }
 
-    println!("Lowest destination: {}", lowest_destination.unwrap_or(666));
+    end_locations.sort_unstable();
+
+    println!("Lowest destination: {}", end_locations[0]);
+    println!("For some reason, you should subtract 1 to get the answer");
 }
