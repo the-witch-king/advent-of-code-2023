0527c0dff9d1b57e96a593101fac0462b2e89ca0
Michael Churilov <michael.churilov@gmail.com>
2024-01-02 20:28:27 -0500

    Day 8 lol

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,4 @@
+use num::integer::lcm;
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -46,7 +47,7 @@ fn get_steps_to_zzz(direcitions: String, network_map: HashMap<String, NetworkNod
                 panic!("Unknown direction: {}", c);
             }
         };
-        if new_node == "ZZZ" {
+        if new_node.ends_with('Z') {
             break;
         }
         current = new_node;
@@ -54,10 +55,59 @@ fn get_steps_to_zzz(direcitions: String, network_map: HashMap<String, NetworkNod
     steps
 }
 
+fn get_steps_to_z(
+    directions: &str,
+    network_map: &HashMap<String, NetworkNode>,
+    starting_point: String,
+) -> u64 {
+    let mut steps = 0;
+    let mut current: &str = &starting_point;
+    for c in directions.chars().cycle() {
+        steps += 1;
+        let new_node = match c {
+            'L' => network_map.get(current).unwrap().left(),
+            'R' => network_map.get(current).unwrap().right(),
+            _ => {
+                panic!("Unknown direction: {}", c);
+            }
+        };
+        if new_node.ends_with('Z') {
+            break;
+        }
+        current = new_node;
+    }
+    steps
+}
+
+fn get_starting_spots(network_map: &HashMap<String, NetworkNode>) -> Vec<String> {
+    network_map
+        .keys()
+        .filter(|k| k.ends_with('A'))
+        .map(|k| k.to_string())
+        .collect()
+}
+
+fn get_steps_to_zzz_for_all_starting_spots(
+    directions: String,
+    network_map: HashMap<String, NetworkNode>,
+) -> u64 {
+    let spots = get_starting_spots(&network_map);
+    let mut steps: Vec<u64> = Vec::new();
+
+    for spot in spots {
+        steps.push(get_steps_to_z(&directions, &network_map, spot))
+    }
+
+    steps.iter().fold(1, |acc, &b| lcm(acc, b))
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let (directions, network_map) = parse_input(input);
-    println!("Steps: {}", get_steps_to_zzz(directions, network_map));
+    println!(
+        "Steps: {}",
+        get_steps_to_zzz_for_all_starting_spots(directions, network_map)
+    );
 }
 
 #[cfg(test)]
@@ -90,5 +140,26 @@ ZZZ = (ZZZ, ZZZ)";
         let (directions, network_map) = parse_input(input);
 
         assert_eq!(get_steps_to_zzz(directions, network_map), 6);
+    }
+
+    #[test]
+    fn test_parallel_in_6_steps() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        let (directions, network_map) = parse_input(input);
+
+        assert_eq!(
+            get_steps_to_zzz_for_all_starting_spots(directions, network_map),
+            6
+        );
     }
 }
